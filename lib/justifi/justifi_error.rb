081403a297057d938c6fb7de20ea2a6c2374126e
Julio Vaz <jmatheus.vaz@gmail.com>
2025-02-05 15:23:39 -0300

    add: response object to justifi_error class

--- lib/justifi/justifi_error.rb
+++ lib/justifi/justifi_error.rb
@@ -4,15 +4,16 @@ module Justifi
   class Error < StandardError
     attr_reader :response_code
 
-    def initialize(response_code, msg)
+    def initialize(response_code, msg, response: nil)
       super(msg)
       @response_code = response_code
+      @response = response
     end
   end
 
   class InvalidHttpResponseError < Error
     def initialize(response:)
-      super(response.http_status, response.error_message)
+      super(response.http_status, response.error_message, response: response)
     end
   end
 end
